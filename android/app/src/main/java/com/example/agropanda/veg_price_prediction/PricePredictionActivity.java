28c1cc5aeac402e0b7c05d90d5f938bac81d0b58
Manikavasagar Anjanan <it18186048@my.sliit.lk>
2021-08-08 20:51:56 +0530

    init price prediction 1

---
+++ android/app/src/main/java/com/example/agropanda/veg_price_prediction/PricePredictionActivity.java
@@ -0,0 +1,16 @@
+package com.example.agropanda.veg_price_prediction;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.agropanda.R;
+
+public class PricePredictionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_price_prediction);
+    }
+}
